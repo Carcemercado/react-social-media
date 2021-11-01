32fa86fab083eef80bafc5a6680a68ba7ca59307
Carlos Arce <arce.code@gmail.com>
2021-11-01 14:59:09 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import postReducer from "./reducer";
+
+export const UserContext = React.createContext();
+export const PostContext = React.createContext({
+  posts: []
+});
 
 function App() {
+  const initialPostState = React.useContext(PostContext);
+  const [state, dispatch] = React.useReducer(postReducer, initialPostState);
+  const [user, setUser] = React.useState("reed");
+  // const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    document.title = user ? `${user}'s Feed` : "Please login";
+  }, [user]);
+
+  // const handleAddPost = React.useCallback(
+  //   newPost => {
+  //     setPosts([newPost, ...posts]);
+  //   },
+  //   [posts]
+  // );
+
+  if (!user) {
+    return <Login setUser={setUser} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostContext.Provider value={{ state, dispatch }}>
+      <UserContext.Provider value={user}>
+        <Header user={user} setUser={setUser} />
+        <CreatePost
+          user={user}
+          //  handleAddPost={handleAddPost}
+        />
+        <PostList posts={state.posts} />
+      </UserContext.Provider>
+    </PostContext.Provider>
   );
 }
 
