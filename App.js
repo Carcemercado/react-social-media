e177dec81a82b36aaa5ae1fab1038d3cf1dc3349
Carlos Arce <arce.code@gmail.com>
2021-11-05 12:48:25 -0400

    Update username  variables
    Removed standard text and added default with my name.

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export const PostContext = React.createContext({
 function App() {
   const initialPostState = React.useContext(PostContext);
   const [state, dispatch] = React.useReducer(postReducer, initialPostState);
-  const [user, setUser] = React.useState("reed");
+  const [user, setUser] = React.useState("Carlos");
   // const [posts, setPosts] = React.useState([]);
 
   React.useEffect(() => {
